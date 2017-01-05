6e80cd646e614408d1f89d14a94b4e283f46939f
FlowerCake13 <lminami23@punahou.edu>
2017-01-04 17:56:43 -1000

    Update app.js
    space

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*function getBackgroundColor(argument) {
 	console.log(argument);
 }*/
-
+    
 function changeColorOnClick(idName) {
 	console.log("HERE!!")
 	var originalElementColor = document.getElementById('selectedColorName').style.backgroundColor;
