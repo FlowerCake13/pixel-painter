50ed8f58bfeb88e04c452dde8cda12d9c00b3324
FlowerCake13 <lminami23@punahou.edu>
2017-01-11 17:24:16 -1000

    Finished APP.JS

--- js/app.js
+++ js/app.js
@@ -1,26 +1,11 @@
-/*function getBackgroundColor(argument) {
-	console.log(argument);
-}*/
-    
 function changeColorOnClick(idName) {
-	console.log("HERE!!")
 	var originalElementColor = document.getElementById('selectedColorName').style.backgroundColor;
 	console.log(originalElementColor);
 	document.getElementById(idName).style.backgroundColor = originalElementColor;
 }
 
-/*function getBackgroundColor(idName) {
-	console.log(idName);
-	console.log("hey")
-	var element = document.getElementById(idName); 
-	var style = window.getComputedStyle(element,null).getPropertyValue("background-color");
-	console.log(style);
-	document.getElementById('selectedColorName').style.backgroundColor = style;
-}*/
- 
-function colorPicker(color){
-	console.log(color);
-	console.log(document.getElementById('inputc').value)
+function erase(){
+	document.getElementById('inputc').value = "#ffffff";
 }
 
 function changeColorFromPicker(idName){
@@ -30,7 +15,7 @@ function changeColorFromPicker(idName){
 }
 
 function fill() {
-	var originalElementColor = document.getElementById('selectedColorName').style.backgroundColor;
+	var originalElementColor = document.getElementById('inputc').value;
 	var gcname = document.getElementsByClassName("canvas");
 	var i;
 	for (i = 0; i < gcname.length; i++){
@@ -39,12 +24,9 @@ function fill() {
 }
 
 function clearButton() {
-	console.log("affaewfefewauefwfewu")
 	var originalElementColor = document.getElementById('eraser').style.backgroundColor;
 	var gcname = document.getElementsByClassName("canvas");
-	console.log("faufahu")
-	var i;
-	for (i = 0; i < gcname.length; i++){
+	for (var i = 0; i < gcname.length; i++){
 		gcname[i].style.backgroundColor = originalElementColor;
 	}
 }
